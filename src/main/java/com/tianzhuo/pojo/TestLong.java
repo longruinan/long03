d4f0c887c7363c8db1f1db211e6f5c981628463d
龙睿楠 <l1592459986@163.com>
2021-04-07 22:39:42 +0800

    添加项目

---
+++ src/main/java/com/tianzhuo/pojo/TestLong.java
@@ -0,0 +1,13 @@
+package com.tianzhuo.pojo;
+
+/**
+ * @项目: my_test
+ * @包名: com.tianzhuo.pojo
+ * @
+ * @作者: long rui nan
+ * @日期: 2021-04-07 22:23
+ */
+public class TestLong {
+    private String name;
+    private String password;
+}
