f005e3ae777485d4794c8f94c6549f50543caa90
龙睿楠 <l1592459986@163.com>
2021-04-07 22:49:04 +0800

    添加项目功能

---
+++ src/main/java/com/tianzhuo/pojo/LongJian.java
@@ -0,0 +1,12 @@
+package com.tianzhuo.pojo;
+
+/**
+ * @项目: my_test
+ * @包名: com.tianzhuo.pojo
+ * @
+ * @作者: long rui nan
+ * @日期: 2021-04-07 22:46
+ */
+public class LongJian {
+    private String name;
+}
