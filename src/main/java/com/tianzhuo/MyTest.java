09517c115b3b81826712950ef648f287ef01d346
龙睿楠 <l1592459986@163.com>
2021-04-07 22:13:58 +0800

    提交项目

---
+++ src/main/java/com/tianzhuo/MyTest.java
@@ -0,0 +1,14 @@
+package com.tianzhuo;
+
+/**
+ * @项目: my_test
+ * @包名: com.tianzhuo
+ * @
+ * @作者: long rui nan
+ * @日期: 2021-04-02 15:25
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/tianzhuo/TestApplication.java
@@ -0,0 +1,18 @@
+package com.tianzhuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @项目: my_test
+ * @包名: com.tianzhuo
+ * @
+ * @作者: long rui nan
+ * @日期: 2021-04-02 15:10
+ */
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class,args);
+    }
+}
